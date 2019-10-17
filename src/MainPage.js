42f67416e063c5d04e41e576dc869220f13c7d6c
EJlearner <rzrshrp@gmail.com>
2019-10-17 18:53:18 -0400

    setup main picture at top

--- src/MainPage.js
+++ src/MainPage.js
@@ -1,7 +1,24 @@
 import React from "react";
+import mainPicture from "./assets/main-picture.png";
+
+import "./MainPage.css";
 
 function MainPage() {
-  return <div>Tesdsssting</div>;
+  const backgroundStyle = { backgroundImage: `url(${mainPicture})` };
+
+  return (
+    <div className="background-full-height-hack-outer" style={backgroundStyle}>
+      <div className="background-full-height-hack-inner">
+        <div className="top-links">
+          <div>Rainy April Day Productions</div>
+          <div>Synopsis</div>
+          <div>Filmmakers</div>
+          <div>Donate</div>
+          <div>Contact</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default MainPage;
