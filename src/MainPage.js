165de021296d2a474141ed8cc5c2ef7e6e731a2e
EJlearner <rzrshrp@gmail.com>
2019-10-18 19:09:35 -0400

    add watch here section to main page

--- src/MainPage.js
+++ src/MainPage.js
@@ -1,24 +1,35 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import mainPicture from "./assets/main-picture.png";
+import playIcon from "./assets/play-button.png";
 
 import "./MainPage.css";
+import routePaths from "./routePaths";
 
 function MainPage() {
   const backgroundStyle = { backgroundImage: `url(${mainPicture})` };
 
   return (
-    <div className="background-full-height-hack-outer" style={backgroundStyle}>
-      <div className="background-full-height-hack-inner">
-        <div className="top-links">
-          <Link>Rainy April Day Productions</Link>
-          <Link>Synopsis</Link>
-          <Link>Filmmakers</Link>
-          <Link>Donate</Link>
-          <Link>Contact</Link>
+    <>
+      <div className="background-full-height-hack-outer" style={backgroundStyle}>
+        <div className="background-full-height-hack-inner">
+          <div className="top-links">
+            <Link to={routePaths.ABOUT_RADP}>Rainy April Day Productions</Link>
+            <Link to={routePaths.SYNOPSIS}>Synopsis</Link>
+            <Link to={routePaths.FILMMAKERS}>Filmmakers</Link>
+            <Link to={routePaths.DONATE}>Donate</Link>
+            <Link to={routePaths.CONTACT}>Contact</Link>
+          </div>
+          <Link>
+            <div className="trailer-link-area">
+              <div className="watch-trailer-here">Watch Trailer Here</div>
+              <div className="blurred-title">Tipsy</div>
+              <img className="play-icon" alt="play-icon" src={playIcon} />
+            </div>
+          </Link>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
