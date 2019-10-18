026f5f56fbf539733d3b7235c64e54782532f61d
EJlearner <rzrshrp@gmail.com>
2019-10-18 19:10:34 -0400

    add abbout page beginnings

---
+++ src/AboutPage.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+import "./AboutPage.css";
+import TopLinks from "./TopLinks";
+
+function AboutPage() {
+  return (
+    <>
+      <TopLinks />
+      <div className="about-paragraph">
+        <p>
+          Founded in 2019 by April Day, Rainy April Day Productions aspires to have fun telling stories we find
+          entertaining and thought-provoking without pretense or pretention. Our company is committed to developing
+          stories using experimental and traditional filmmaking methods, while championing the traditionally
+          underrepresented filmmaker and providing a platform to produce quality film and television projects. Rainy
+          April Day upholds that genre storytelling, particularly horror, has a place in cinematic expression. Our work
+          embraces the belief that even the most ordinary experiences can become unique and powerful stories when told
+          from our diverse perspectives.
+        </p>
+        <p>Founder/CEO=April Day</p>
+        <p>President=Earl Jones</p>
+      </div>
+    </>
+  );
+}
+
+export default AboutPage;
--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+
 import MainPage from "./MainPage";
+import AboutPage from "./AboutPage";
 
 import "./App.css";
+import routePaths from "./routePaths";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Switch>
-          <Route component={MainPage} path={"/"} />
+          <Route component={MainPage} path={routePaths.MAIN} />
+          <Route component={AboutPage} path={routePaths.ABOUT_RADP} />
+
           <Route component={MainPage} />
         </Switch>
       </BrowserRouter>
