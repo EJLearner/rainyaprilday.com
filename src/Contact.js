c87d44dafc3af52420812b3b30ebc1f529f8f4af
EJlearner <rzrshrp@gmail.com>
2019-10-18 21:17:31 -0400

    add more details to contacts

--- src/Contact.js
+++ src/Contact.js
@@ -7,7 +7,17 @@ function Contact() {
   return (
     <>
       <TopLinks />
-      Contact Info
+      <div className="contact-page">
+        <h1>Contact</h1>
+        Rainy April Day Productions
+        <br />
+        PO Box 26602
+        <br />
+        Gwynn Oak, MD 21207
+        <br />
+        <a href="mailto:aprild@rainyaprilday.com">AprilD@rainyaprilday.com</a>
+        <br />
+      </div>
     </>
   );
 }
