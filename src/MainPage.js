d525574f398545f20a5be2319a34a3927985c9f6
EJlearner <rzrshrp@gmail.com>
2019-10-18 20:50:02 -0400

    simplify styling some

--- src/MainPage.js
+++ src/MainPage.js
@@ -9,31 +9,29 @@ import routePaths from "./routePaths";
 function MainPage() {
   const backgroundStyle = { backgroundImage: `url(${mainPicture})` };
 
-  let trailerLink;
+  let trailerLink = "something";
 
   return (
-    <>
-      <div className="background-full-height-hack-outer" style={backgroundStyle}>
-        <div className="background-full-height-hack-inner">
-          <div className="top-links">
-            <NavLink to={routePaths.ABOUT_RADP}>Rainy April Day Productions</NavLink>
-            <NavLink to={routePaths.SYNOPSIS}>Synopsis</NavLink>
-            <NavLink to={routePaths.FILMMAKERS}>Filmmakers</NavLink>
-            <NavLink to={routePaths.DONATE}>Donate</NavLink>
-            <NavLink to={routePaths.CONTACT}>Contact</NavLink>
-          </div>
-          {trailerLink && (
-            <Link to={trailerLink}>
-              <div className="trailer-link-area">
-                <div className="watch-trailer-here">Watch Trailer Here</div>
-                <div className="blurred-title">Tipsy</div>
-                <img className="play-icon" alt="play-icon" src={playIcon} />
-              </div>
-            </Link>
-          )}
+    <div className="main-page-and-background" style={backgroundStyle}>
+      <div className="gradient-mask">
+        <div className="top-links">
+          <NavLink to={routePaths.ABOUT_RADP}>Rainy April Day Productions</NavLink>
+          <NavLink to={routePaths.SYNOPSIS}>Synopsis</NavLink>
+          <NavLink to={routePaths.FILMMAKERS}>Filmmakers</NavLink>
+          <NavLink to={routePaths.DONATE}>Donate</NavLink>
+          <NavLink to={routePaths.CONTACT}>Contact</NavLink>
         </div>
+        {trailerLink && (
+          <Link to={trailerLink}>
+            <div className="trailer-link-area">
+              <div className="watch-trailer-here">Watch Trailer Here</div>
+              <div className="blurred-title">Tipsy</div>
+              <img className="play-icon" alt="play-icon" src={playIcon} />
+            </div>
+          </Link>
+        )}
       </div>
-    </>
+    </div>
   );
 }
 
