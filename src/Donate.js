252f948f051505363e0bbecf985ac1da33416fc7
EJlearner <rzrshrp@gmail.com>
2019-10-19 23:47:42 -0400

    add content to donate page and put dummy text on synopsis

--- src/Donate.js
+++ src/Donate.js
@@ -2,9 +2,38 @@ import React from "react";
 
 import "./Donate.css";
 import withPageWrapper from "./withPageWrapper";
+import officersPicture from "./assets/officers-shot.png";
+
+function renderSetAmounts() {
+  const amounts = [50, 100, 250, 500, 1000];
+
+  const renderedSetAmounts = amounts.map(amount => {
+    return (
+      <a className="set-amount-link" href="" key={amount}>
+        ${amount}
+      </a>
+    );
+  });
+
+  return renderedSetAmounts;
+}
 
 function Donate() {
-  return <>Donate Info</>;
+  return (
+    <>
+      <img alt="Officers Carr and Holden movie scene" className="movie-picture" src={officersPicture} />
+      <p>
+        <span className="first-sentence">What am I donating to?</span> The filmmakers are raising money to film the
+        entire feature film <span className="title">Tipsy</span>. Shooting is scheduled to begin early October 2020 and
+        wrap late November 2020. All donations will go toward pre-production, production, and post production costs
+        associated with completing the feature film. Remaining proceeds will be used to cover entrance fees to various
+        film festivals.
+      </p>
+
+      <h2 className="gift-amount-title">Select your gift amount:</h2>
+      <div className="gift-amount-choices">{renderSetAmounts()}</div>
+    </>
+  );
 }
 
 export default withPageWrapper(Donate);
--- src/Synopsis.js
+++ src/Synopsis.js
@@ -4,7 +4,16 @@ import "./Synopsis.css";
 import withPageWrapper from "./withPageWrapper";
 
 function Synopsis() {
-  return <div>Synopsis Info</div>;
+  return (
+    <div>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores soluta magni nemo doloribus, accusantium nobis
+      doloremque earum iusto ab impedit iste tenetur adipisci id. Aspernatur minus commodi tempore iure tenetur?</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores soluta magni nemo doloribus, accusantium nobis
+      doloremque earum iusto ab impedit iste tenetur adipisci id. Aspernatur minus commodi tempore iure tenetur?</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores soluta magni nemo doloribus, accusantium nobis
+      doloremque earum iusto ab impedit iste tenetur adipisci id. Aspernatur minus commodi tempore iure tenetur?</p>
+    </div>
+  );
 }
 
 export default withPageWrapper(Synopsis);
