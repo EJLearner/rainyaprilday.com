f7f2d2b529416c4c547747bef1807aa7a9507338
EJlearner <rzrshrp@gmail.com>
2019-10-29 21:44:48 -0400

    mostly finish filmmakers page styling

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Contact from "./Contact";
 import "./App.css";
 import routePaths from "./routePaths";
 import Donate from "./Donate";
-import Filmmakers from "./Filmmakers";
+import FilmmakersPage from "./FilmmakersPage";
 import Synopsis from "./Synopsis";
 
 function App() {
@@ -20,7 +20,7 @@ function App() {
           <Route component={AboutPage} path={routePaths.ABOUT_RADP} />
           <Route component={Contact} path={routePaths.CONTACT} />
           <Route component={Donate} path={routePaths.DONATE} />
-          <Route component={Filmmakers} path={routePaths.FILMMAKERS} />
+          <Route component={FilmmakersPage} path={routePaths.FILMMAKERS} />
           <Route component={Synopsis} path={routePaths.SYNOPSIS} />
 
           <Route component={MainPage} />
---
+++ src/Filmmaker.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import "./Filmmaker.css";
+
+function Filmmaker(props) {
+  return (
+    <div className="name-and-bio">
+      <div className="headshot-and-name-column">
+        <div className="headshot-and-name">
+          <img alt={props.namae} src={props.imageSource} />
+          <div className="behind-text-mask"></div>
+          <div className="filmmaker-name">{props.name}</div>
+        </div>
+      </div>
+      <div className="filmmaker-bio">{props.children}</div>
+    </div>
+  );
+}
+
+export default Filmmaker;
