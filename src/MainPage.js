d5113e2eb680bf1430b71a800574cb00bd2b4498
EJlearner <rzrshrp@gmail.com>
2019-10-30 19:02:00 -0400

    clean up code

--- src/MainPage.js
+++ src/MainPage.js
@@ -9,19 +9,15 @@ import TopLinks from "./TopLinks";
 function MainPage() {
   const backgroundStyle = { backgroundImage: `url(${mainPicture})` };
 
-  let trailerLink = "something";
-
   return (
     <div className="main-page-and-background" style={backgroundStyle}>
       <div className="gradient-mask">
         <TopLinks isHome />
-        {trailerLink && (
-          <a className="trailer-link-area" href="https://vimeo.com/368163654/ba50ac3309">
-            <div className="watch-trailer-here">Watch Trailer Here</div>
-            <div className="blurred-title">Tipsy</div>
-            <img className="play-icon" alt="play-icon" src={playIcon} />
-          </a>
-        )}
+        <a className="trailer-link-area" href="https://vimeo.com/368163654/ba50ac3309">
+          <div className="watch-trailer-here">Watch Trailer Here</div>
+          <div className="blurred-title">Tipsy</div>
+          <img className="play-icon" alt="play-icon" src={playIcon} />
+        </a>
       </div>
     </div>
   );
